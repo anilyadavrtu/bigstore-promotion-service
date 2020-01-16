dfebc93355cd3159e66c97f1684f001ff5498706
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 19:44:44 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstorepromotionservice/BigstorePromotionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorepromotionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstorePromotionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstorePromotionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigstore/bigstorepromotionservice/BigstorePromotionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorepromotionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigstorePromotionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
